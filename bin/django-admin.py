26913c61338d7f5007f0230f677e54386dc6ad29
ewerton <joaosouza@alunos.utfpr.edu.br>
2019-04-30 11:16:33 -0300

    Uplod da Aplicação

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/ewerton/Marina/UTFPR/Disciplinas/Tecnologias em Desenvolvimento de Sistemas/Seminario/HelloWorld/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
